1fa281559bcb556076ea0ce185548ea43e0dc1dc
sebastiant7575 <sebastiant7575@gmail.com>
2025-08-25 17:35:44 -0400

    feat: hero page v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,24 @@ export default function Home() {
   return (
     <div id="first" className="h-screen snap-y snap-mandatory">
       <div className="h-screen flex flex-col items-center justify-center snap-start bg-offwhite">
-        <h2 className="text-4xl font-bold">Home</h2>
-        <p className="mt-2">Scroll Down</p>
+          <div className="relative isolate px-6 pt-14 lg:px-8">
+   
+    <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+      <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+        <div className="relative rounded-full px-3 py-1 text-sm/6 text-black ring-1 ring-black/10 hover:ring-black/20">
+          Announcing our latest business. <a href="#" className="font-semibold text-indigo-400"><span aria-hidden="true" className="absolute inset-0"></span>Read more <span aria-hidden="true">&rarr;</span></a>
+        </div>
+      </div>
+      <div className="text-center">
+        <h1 className="text-5xl font-semibold tracking-tight text-balance text-blacksm:text-7xl">Here to enrich your online business</h1>
+        <p className="mt-8 text-lg font-medium text-pretty text-gray-700 sm:text-xl/8">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.</p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <a href="#" className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Get started</a>
+          <a href="#" className="text-sm/6 font-semibold text-black">Learn more <span aria-hidden="true">→</span></a>
+        </div>
+      </div>
+    </div>
+  </div>
       </div>
 
       <div id="second" className="h-screen flex flex-col items-center justify-center snap-start bg-darkgreen text-offwhite">
