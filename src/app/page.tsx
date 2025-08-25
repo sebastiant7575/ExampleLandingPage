2612720ab98b25de294485ab6126f1a80782e645
sebastiant7575 <sebastiant7575@gmail.com>
2025-08-25 19:58:14 -0400

    feat: changed font to roboto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
             </div>
             <div className="text-center">
               <h1 className="text-5xl font-semibold tracking-tight text-balance text-blacksm:text-7xl">Here to enrich your online business</h1>
-              <p className="mt-8 text-lg font-medium text-pretty text-gray-700 sm:text-xl/8">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.</p>
+              <p className="mt-8 text-lg font-medium text-pretty text-gray-600 sm:text-xl/8">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.</p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a href="#third" className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Get started</a>
                 <a href="#second" className="text-sm/6 font-semibold text-black">Learn more <span aria-hidden="true">→</span></a>
