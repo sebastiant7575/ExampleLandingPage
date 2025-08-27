fe0960ca531ea70546c119c4b3185f1ef64a8cb1
sebastiant7575 <sebastiant7575@gmail.com>
2025-08-27 14:06:46 -0400

    chore: fixing site issues for responsiveness and visuals

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tailwindcss.com',
+        port: '',          // leave empty unless needed
+        pathname: '/plus-assets/img/component-images/**', // allow this path
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/components/pages/HeroPage.tsx
+++ src/components/pages/HeroPage.tsx
@@ -1,11 +1,11 @@
 const HeroPage = () => {
   return (
-  	<div className="h-screen flex flex-col items-center justify-center snap-start bg-primary">
+  	<div className="h-screen flex flex-col items-center justify-center bg-primary">
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm/6 text-black ring-1 ring-black/10 hover:ring-black/20">
-              Announcing our latest business.&nbsp;
+              Announcing our latest designs.&nbsp;
               <a href="#second" className="font-semibold text-accent-light">
                 <span aria-hidden="true" className="absolute inset-0"></span>Read more&nbsp;
                 <span aria-hidden="true">&rarr;</span>
@@ -13,13 +13,12 @@ const HeroPage = () => {
             </div>
           </div>
           <div className="text-center">
-            {/* <h1 className="font-semibold tracking-tight text-balance text-black sm:text-7xl">Here to enrich your online business</h1> */}
-						<h1 className="font-semibold tracking-tight text-balance text-black text-4xl leading-tight sm:text-7xl sm:leading-[1.1]">
-							Here to enrich your online business
+						<h1 className="font-semibold tracking-tight text-balance text-black text-4xl leading-tight md:text-7xl md:leading-[1.1]">
+							Here to <span className="text-accent underline font-bold">design</span> <span className="">your</span> online business
 						</h1>
             <p className="mt-8 text-lg font-medium text-pretty text-gray-600 sm:text-xl/8">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.</p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a href="#third" className="rounded-md bg-accent px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-accent-light focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Get started</a>
+              <a href="#third" className="rounded-md bg-accent px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-accent-light focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Contact us</a>
               <a
                 href="#second"
                 className="relative text-sm/6 font-semibold text-black group"
