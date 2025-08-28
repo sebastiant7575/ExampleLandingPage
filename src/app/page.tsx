8004d98adc85ad65c3fd24d28a1d86df8c99dd6f
sebastiant7575 <sebastiant7575@gmail.com>
2025-08-27 20:34:42 -0400

    feat: svg background + image changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,40 +10,40 @@ export default function Home() {
       {/* Services */}
       <ServicesPage />
       {/* About Us */}
-      <div id="third" className="h-screen flex flex-col items-center justify-center bg-primary text-black">
+      <div id="third" className="h-[50vh] flex flex-col items-center justify-center bg-primary text-black">
         <h2 className="font-bold text-balance">About Us</h2>
         <p className="mt-2 text-pretty">Scroll Down</p>
       </div>
       {/* Service Highlights */}
-      <div id="fourth" className="h-screen flex flex-col items-center justify-center bg-accent text-white">
+      <div id="fourth" className="h-[50vh] flex flex-col items-center justify-center bg-accent text-white">
         <h2 className="font-bold text-balance">Footer</h2>
         <p className="mt-2 text-pretty">Scroll Up</p>
       </div>
       {/* Gallery */}
-      <div id="fourth" className="h-screen flex flex-col items-center justify-center bg-primary text-white">
+      {/* <div id="fourth" className="h-screen flex flex-col items-center justify-center bg-primary text-white">
         <h2 className="font-bold text-balance">Footer</h2>
         <p className="mt-2 text-pretty">Scroll Up</p>
-      </div>
+      </div> */}
       {/* Why Choose Us*/}
-      <div id="fourth" className="h-screen flex flex-col items-center justify-center bg-accent text-white">
+      {/* <div id="fourth" className="h-screen flex flex-col items-center justify-center bg-accent text-white">
         <h2 className="font-bold text-balance">Footer</h2>
         <p className="mt-2 text-pretty">Scroll Up</p>
-      </div>
+      </div> */}
       {/* FAQ */}
-      <div id="fourth" className="h-screen flex flex-col items-center justify-center bg-primary text-white">
+      {/* <div id="fourth" className="h-screen flex flex-col items-center justify-center bg-primary text-white">
         <h2 className="font-bold text-balance">Footer</h2>
         <p className="mt-2 text-pretty">Scroll Up</p>
-      </div>
+      </div> */}
       {/* Reviews */}
-      <div id="fourth" className="h-screen flex flex-col items-center justify-center bg-accent text-white">
+      {/* <div id="fourth" className="h-screen flex flex-col items-center justify-center bg-accent text-white">
         <h2 className="font-bold text-balance">Footer</h2>
         <p className="mt-2 text-pretty">Scroll Up</p>
-      </div>
+      </div> */}
       {/* Call To Action */}
-      <div id="fourth" className="h-screen flex flex-col items-center justify-center bg-primary text-white">
+      {/* <div id="fourth" className="h-screen flex flex-col items-center justify-center bg-primary text-white">
         <h2 className="font-bold text-balance">Footer</h2>
         <p className="mt-2 text-pretty">Scroll Up</p>
-      </div>         
+      </div>          */}
     </div>
   );
 }
