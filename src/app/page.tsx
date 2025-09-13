4f8cc043a9a7f3febe91824644f4b9759e1ea0d4
sebastiant7575 <sebastiant7575@gmail.com>
2025-09-13 15:00:35 -0400

    feat: fix responsiveness, add sections, TODO: fix links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,51 +16,25 @@ export default function Home() {
       {/* Services */}
       <ServicesPage />
       {/* About Us */}
-      <div id="third" className="h-screen flex flex-col items-center justify-center bg-primary text-black">
+      <div id="third" className="h-[50vh] flex flex-col items-center justify-center bg-primary text-black">
         <h2 className="font-bold text-balance">About Us</h2>
-        {/* <p className="mt-2 text-pretty">Scroll Down</p> */}
+        <p className="mt-2 text-pretty">header on left + text, photo on right, 2 column layout</p>
       </div>
       {/* Service Highlights */}
       <div id="fourth" className="h-screen flex flex-col items-center justify-center bg-accent text-white">
-        <h2 className="font-bold text-balance">Contact Us</h2>
-        {/* <div className="">
-          <h2 className="font-bold text-balance underline">Your Company</h2>
-        </div>
-        <div className="grid grid-cols-3">
-          <div><h3>Contact</h3></div>
-          <div><h3>Contact</h3></div>
-          <div><h3>Contact</h3></div> */}
-          {/* <div className="flex flex-col justify-center">
-            <h3 className="font-bold">Contact</h3>
-            <p>{`444-444-4444`}</p>
-            <p>{`email@email.com`}</p>
-            <p>{`City, Province/State`}</p>
-          </div>
-          <div className="flex flex-col justify-center">
-            <h3 className="font-bold">Contact</h3>
-            <p>{`444-444-4444`}</p>
-            <p>{`email@email.com`}</p>
-            <p>{`City, Province/State`}</p>
-          </div>
-          <div className="flex flex-col justify-center">
-            <h3 className="font-bold">Contact</h3>
-            <p>{`444-444-4444`}</p>
-            <p>{`email@email.com`}</p>
-            <p>{`City, Province/State`}</p>
-          </div> */}
-        {/* </div> */}
-        
+        <h2 className="font-bold text-balance">Service Highlights</h2>
+        {/* <p className="mt-2 text-pretty">Scroll Down</p> */}
       </div>
-      {/* Gallery */}
-      {/* <div id="fourth" className="h-screen flex flex-col items-center justify-center bg-primary text-white">
-        <h2 className="font-bold text-balance">Footer</h2>
-        <p className="mt-2 text-pretty">Scroll Up</p>
-      </div> */}
-      {/* Why Choose Us*/}
-      {/* <div id="fourth" className="h-screen flex flex-col items-center justify-center bg-accent text-white">
-        <h2 className="font-bold text-balance">Footer</h2>
-        <p className="mt-2 text-pretty">Scroll Up</p>
-      </div> */}
+      {/* Gallery wth example sites */}
+      <div id="fifth" className="h-[50vh] flex flex-col items-center justify-center bg-primary text-black">
+        <h2 className="font-bold text-balance">Gallery</h2>
+        <p className="mt-2 text-pretty">Carousel of sites</p>
+      </div>
+      {/* Pricing / Buy / Contact Us (Call to Action) */}
+      <div id="sixth" className="min-h-screen flex flex-col items-center justify-center bg-accent text-white">
+        <h2 className="font-bold text-balance">Pricing</h2>
+        <p className="mt-2 text-pretty">Cards left to right</p>
+      </div>
       {/* FAQ */}
       {/* <div id="fourth" className="h-screen flex flex-col items-center justify-center bg-primary text-white">
         <h2 className="font-bold text-balance">Footer</h2>
@@ -72,10 +46,20 @@ export default function Home() {
         <p className="mt-2 text-pretty">Scroll Up</p>
       </div> */}
       {/* Call To Action */}
-      {/* <div id="fourth" className="h-screen flex flex-col items-center justify-center bg-primary text-white">
+      {/* <div id="seventh" className="h-screen flex flex-col items-center justify-center bg-primary text-white">
         <h2 className="font-bold text-balance">Footer</h2>
         <p className="mt-2 text-pretty">Scroll Up</p>
       </div>          */}
+      {/* Pricing Tiers*/}
+      {/* <div id="fourth" className="h-screen flex flex-col items-center justify-center bg-primary text-white">
+        <h2 className="font-bold text-balance">Footer</h2>
+        <p className="mt-2 text-pretty">Scroll Up</p>
+      </div> */}
+      {/* Footer */}
+      <div id="seventh" className="h-screen flex flex-col items-center justify-center bg-primary text-white">
+        <h2 className="font-bold text-balance">Footer</h2>
+        <p className="mt-2 text-pretty">Scroll Up</p>
+      </div>  
     </div>
   );
 }
--- src/components/pages/ServicesPage.tsx
+++ src/components/pages/ServicesPage.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 const ServicesPage = () => {
   return (      
-    <div id="second" className="min-h-screen lg:max-h-screen flex flex-col items-center justify-center bg-accent text-white">
+    <div id="second" className="min-h-screen flex flex-col items-center justify-center bg-accent text-white">
       <div className="px-12 pb-12 lg:pb-0 sm:mt-12 lg:mt-6">
           <p className="mx-auto mt-8 lg:mt-0 max-w-lg text-center text-4xl font-semibold tracking-tight text-balance text-white sm:text-5xl">
             <span className="underline">Everything</span> you need to market your product
@@ -12,18 +12,19 @@ const ServicesPage = () => {
               <div className="absolute inset-px rounded-lg bg-primary lg:rounded-l-4xl"></div>
               <div className="relative flex h-[450px] lg:h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] lg:rounded-l-[calc(2rem+1px)] shadow-md">
                 <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0 group">
-                  <p className="mt-2 text-xl font-medium tracking-tight text-black max-lg:text-center">Mobile friendly</p>
-                  <p className="relative mt-2 max-w-lg text-sm/6 text-pretty text-gray-600 max-lg:text-center">
-                    {`All our designs are incredibily responsive to screen sizes, whether you're viewing on the web or mobile, our sites look just right.`}
+                  <p className="relative mt-2 text-xl font-medium tracking-tight text-black max-lg:text-center">
+                    Mobile friendly
                     <span
-                      className="absolute left-0 -bottom-0.5 h-0.5 w-0 bg-accent transition-all duration-300 group-hover:w-full"
+                      className="absolute left-0 -bottom-0.5 h-0.5 w-0 bg-accent transition-all duration-300 group-hover:max-md:w-0 group-hover:lg:w-32"
                     ></span>
+                  </p>
+                  <p className="relative mt-2 max-w-lg text-sm/6 text-pretty text-gray-600 max-lg:text-center">
+                    {`All our designs are incredibily responsive to screen sizes, whether you're viewing on the web or mobile, our sites look just right. Try resizing.`}
                   </p>
                 </div>
                 <div className="@container relative min-h-120 w-full grow max-lg:mx-auto max-lg:max-w-sm">
                   <div className="absolute inset-x-10 top-10 bottom-0 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 outline outline-white/20">
                     <Image
-                      // src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-mobile-friendly.png"
                       src={'/iphone2.png'}
                       alt="Mobile friendly"
                       fill
@@ -38,12 +39,14 @@ const ServicesPage = () => {
               <div className="absolute inset-px rounded-lg bg-primary max-lg:rounded-t-4xl"></div>
               <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
                 <div className="px-8 pt-8 sm:px-10 sm:pt-10 group">
-                  <p className="mt-2 text-xl font-medium tracking-tight text-black max-lg:text-center">Performance</p>
+                  <p className="relative mt-2 text-xl font-medium tracking-tight text-black max-lg:text-center">
+                    Performance
+                    <span
+                      className="absolute left-0 -bottom-0.5 h-0.5 w-0 bg-accent transition-all duration-300 group-hover:max-md:w-0 group-hover:lg:w-28"
+                    ></span>
+                  </p>
                   <p className="relative mt-2 max-w-lg text-sm/6 text-pretty text-gray-600 max-lg:text-center">
                     Our sites load FAST, so everyone can access and view your product instantly.
-                    <span
-                      className="absolute left-0 -bottom-0.5 h-0.5 w-0 bg-accent transition-all duration-300 group-hover:w-full"
-                    ></span>
                   </p>
                 </div>
               <div className="flex flex-1 flex-col px-8 max-lg:pt-10 max-lg:pb-12 sm:px-10 lg:pb-2">
@@ -64,12 +67,14 @@ const ServicesPage = () => {
               <div className="absolute inset-px rounded-lg bg-primary"></div>
               <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)]">
                 <div className="px-8 pt-8 sm:px-10 sm:pt-10 group">
-                  <p className="mt-2 text-xl font-medium tracking-tight text-black max-lg:text-center">Maintenance & Hosting</p>
+                  <p className="relative mt-2 text-xl font-medium tracking-tight text-black max-lg:text-center">
+                    Maintenance & Hosting
+                    <span
+                      className="absolute left-0 -bottom-0.5 h-0.5 w-0 bg-accent transition-all duration-300 group-hover:max-md:w-0 lg:group-hover:w-50"
+                    ></span>
+                  </p>
                   <p className="relative mt-2 max-w-lg text-sm/6 text-pretty text-gray-600 max-lg:text-center">
                     {`We can take care of hosting your site and maintenance, so everything "just works", and you can focus on growing your business. No worrying about SSL Certificates, downtime, or security.`}
-                    <span
-                      className="absolute left-0 -bottom-0.5 h-0.5 w-0 bg-accent transition-all duration-300 group-hover:w-full"
-                    ></span>
                   </p>
                 </div>
               <div className="@container flex flex-1 items-center max-lg:py-6 lg:pb-2">
@@ -90,16 +95,18 @@ const ServicesPage = () => {
               <div className="absolute inset-px rounded-lg bg-primary max-lg:rounded-b-4xl lg:rounded-r-4xl"></div>
               <div className="relative flex h-[450px] lg:h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
                 <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0 group">
-                  <p className="mt-2 text-xl font-medium tracking-tight text-black max-lg:text-center">Modern Designs</p>
-                  <p className="relative mt-2 max-w-lg text-pretty text-sm/6 text-gray-600 max-lg:text-center">
-                    {`We're always keeping up to date on the latest design trends - so your site looks sleek and stylish.`}
+                  <p className="relative mt-2 text-xl font-medium tracking-tight text-black max-lg:text-center">
+                    Modern Designs
                     <span
-                      className="absolute left-0 -bottom-0.5 h-0.5 w-0 bg-accent transition-all duration-300 group-hover:w-full"
+                      className="absolute left-0 -bottom-0.5 h-0.5 w-0 bg-accent transition-all duration-300 group-hover:max-md:w-0 group-hover:lg:w-35"
                     ></span>
                   </p>
+                  <p className="relative mt-2 max-w-lg text-pretty text-sm/6 text-gray-600 max-lg:text-center">
+                    {`We're always keeping up to date on the latest design trends - so your site looks sleek and stylish.`}
+                  </p>
                 </div>
-                <div className="relative min-h-120 w-full grow">
-                  <div className="absolute top-10 right-10 bottom-[1px] left-10 overflow-hidden rounded-t-xl bg-gray-600/60 outline outline-white/10">
+                <div className="relative w-full grow">
+                  <div className="absolute top-10 right-10 bottom-10 left-10 overflow-hidden rounded-xl bg-gray-600/60 outline outline-white/10">
                     <Image
                       src="/moderndesign.jpeg"
                       alt="Modern design"
