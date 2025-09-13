327a9c19ed24873c6fde60495167a98044d9027e
sebastiant7575 <sebastiant7575@gmail.com>
2025-09-13 15:27:00 -0400

    chore: add prettier and re-format files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,9 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Landing Page",
   description: "This is an example landing page",
-}
+};
 
 export default function Home() {
-
   return (
     <div>
       {/* Hero Page*/}
@@ -16,22 +15,36 @@ export default function Home() {
       {/* Services */}
       <ServicesPage />
       {/* About Us */}
-      <div id="third" className="h-[50vh] flex flex-col items-center justify-center bg-primary text-black">
+      <div
+        id="third"
+        className="h-[50vh] flex flex-col items-center justify-center bg-primary text-black"
+      >
         <h2 className="font-bold text-balance">About Us</h2>
-        <p className="mt-2 text-pretty">header on left + text, photo on right, 2 column layout</p>
+        <p className="mt-2 text-pretty">
+          header on left + text, photo on right, 2 column layout
+        </p>
       </div>
       {/* Service Highlights */}
-      <div id="fourth" className="h-screen flex flex-col items-center justify-center bg-accent text-white">
+      <div
+        id="fourth"
+        className="h-screen flex flex-col items-center justify-center bg-accent text-white"
+      >
         <h2 className="font-bold text-balance">Service Highlights</h2>
         {/* <p className="mt-2 text-pretty">Scroll Down</p> */}
       </div>
       {/* Gallery wth example sites */}
-      <div id="fifth" className="h-[50vh] flex flex-col items-center justify-center bg-primary text-black">
+      <div
+        id="fifth"
+        className="h-[50vh] flex flex-col items-center justify-center bg-primary text-black"
+      >
         <h2 className="font-bold text-balance">Gallery</h2>
         <p className="mt-2 text-pretty">Carousel of sites</p>
       </div>
       {/* Pricing / Buy / Contact Us (Call to Action) */}
-      <div id="sixth" className="min-h-screen flex flex-col items-center justify-center bg-accent text-white">
+      <div
+        id="sixth"
+        className="min-h-screen flex flex-col items-center justify-center bg-accent text-white"
+      >
         <h2 className="font-bold text-balance">Pricing</h2>
         <p className="mt-2 text-pretty">Cards left to right</p>
       </div>
@@ -56,10 +69,13 @@ export default function Home() {
         <p className="mt-2 text-pretty">Scroll Up</p>
       </div> */}
       {/* Footer */}
-      <div id="seventh" className="h-screen flex flex-col items-center justify-center bg-primary text-white">
+      <div
+        id="seventh"
+        className="h-screen flex flex-col items-center justify-center bg-primary text-white"
+      >
         <h2 className="font-bold text-balance">Footer</h2>
         <p className="mt-2 text-pretty">Scroll Up</p>
-      </div>  
+      </div>
     </div>
   );
 }
--- src/components/pages/HeroPage.tsx
+++ src/components/pages/HeroPage.tsx
@@ -1,6 +1,21 @@
 const HeroPage = () => {
   return (
-  	<div id="first" className="relative min-h-[75vh] bg-[url('/graph-paper.svg')] text-accent bg-repeat bg-[length:50px_50px] flex flex-col items-center justify-center bg-primary">
+    <div
+      id="first"
+      className="
+        relative 
+        min-h-[75vh] 
+        bg-[url('/graph-paper.svg')] 
+        text-accent 
+        bg-repeat 
+        bg-[length:50px_50px] 
+        flex 
+        flex-col 
+        items-center 
+        justify-center 
+        bg-primary
+      "
+    >
       <div className="absolute inset-0 bg-white/80"></div>
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div className="mx-auto max-w-2xl py-8">
@@ -8,33 +23,42 @@ const HeroPage = () => {
             <div className="relative rounded-full px-3 py-1 text-sm/6 text-black ring-1 ring-black/30 hover:ring-black/70">
               Announcing our latest designs.&nbsp;
               <a href="#second" className="font-semibold text-accent-light">
-                <span aria-hidden="true" className="absolute inset-0"></span>Read more&nbsp;
+                <span aria-hidden="true" className="absolute inset-0"></span>
+                Read more&nbsp;
                 <span aria-hidden="true">&rarr;</span>
               </a>
             </div>
           </div>
           <div className="text-center">
-						<h1 className="font-semibold tracking-tight text-balance text-black text-4xl leading-tight md:text-7xl md:leading-[1.1]">
-							Here to <span className="text-accent underline font-bold">design</span> <span className="">your</span> online business
-						</h1>
-            <p className="mt-8 text-lg font-medium text-pretty text-gray-600 sm:text-xl/8">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.</p>
+            <h1 className="font-semibold tracking-tight text-balance text-black text-4xl leading-tight md:text-7xl md:leading-[1.1]">
+              Here to{" "}
+              <span className="text-accent underline font-bold">design</span>{" "}
+              <span className="">your</span> online business
+            </h1>
+            <p className="mt-8 text-lg font-medium text-pretty text-gray-600 sm:text-xl/8">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.
+            </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a href="#second" className="rounded-md bg-accent px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-accent-light focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Contact us</a>
+              <a
+                href="#second"
+                className="rounded-md bg-accent px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-accent-light focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              >
+                Contact us
+              </a>
               <a
                 href="#second"
                 className="relative text-sm/6 font-semibold text-black group"
               >
                 Learn more <span aria-hidden="true">→</span>
-                <span
-                  className="absolute left-0 -bottom-0.5 h-0.5 w-0 bg-accent transition-all duration-300 group-hover:w-full"
-                ></span>
+                <span className="absolute left-0 -bottom-0.5 h-0.5 w-0 bg-accent transition-all duration-300 group-hover:w-full"></span>
               </a>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeroPage
+export default HeroPage;
